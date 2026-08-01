675dd48147e939e1fbc2782e6a19f98d3396c73e
ARAlsalhi <aalsalhi.eng@gmail.com>
2026-08-01 18:45:56 +0300

    add auth endpoint and add implement JWT auth

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -1,32 +1,31 @@
 from flask import Flask
 from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
 from flask_restx import Api
-from app.api.v1.users import api as users_ns
-from app.api.v1.amenities import api as amenities_ns
-from app.api.v1.places import api as places_ns
-from app.api.v1.reviews import api as reviews_ns
-
 
 bcrypt = Bcrypt()
+jwt = JWTManager()
+
 
 def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
 
-    app.config.from_object(config_class) , """Now the defult configuration is set to DevelopmentConfig"""
+    app.config.from_object(config_class) # Now the defult configuration is set to DevelopmentConfig
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/')
 
     bcrypt.init_app(app)  # Initialize Bcrypt with the Flask app
+    jwt.init_app(app)  # Initialize JWTManager with the Flask app
 
     from app.api.v1.users import api as users_ns
-    api.add_namespace(users_ns, path='/api/v1/users')
-
+    from app.api.v1.auth import api as auth_ns
     from app.api.v1.amenities import api as amenities_ns
-    api.add_namespace(amenities_ns, path='/api/v1/amenities')
-
     from app.api.v1.places import api as places_ns
-    api.add_namespace(places_ns, path='/api/v1/places')
-
     from app.api.v1.reviews import api as reviews_ns
+
+    api.add_namespace(users_ns, path='/api/v1/users')
+    api.add_namespace(auth_ns, path='/api/v1/auth')
+    api.add_namespace(amenities_ns, path='/api/v1/amenities')
+    api.add_namespace(places_ns, path='/api/v1/places')
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
 
     return app
